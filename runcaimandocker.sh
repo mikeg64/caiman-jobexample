cf23307af20c8e7da95708ed81cb3beac8e6aa77
mikeg64 <m.griffiths@sheffield.ac.uk>
2022-03-28 14:43:08 +0100

    Update runcaimandocker.sh

--- runcaimandocker.sh
+++ runcaimandocker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run -v $PWD:/home/caiman caiman:4.0  /caiman-matlab/run_caiman_standalone.sh /opt/mcr/v910
+#https://hub.docker.com/repository/docker/mikeg64/caiman
+#Either 
+# 1. copy and paste the command below 
+# 2. run the script file directly from the  folder containing the jobfile
+# May need to update the $PWD with the directory path for the working directory
+
+docker run -v $PWD:/home/caiman mikeg64/caiman:4.0  /caiman-matlab/run_caiman_standalone.sh /opt/mcr/v910
